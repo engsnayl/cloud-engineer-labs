b9e92ec9f914daefbb0a6b9b80e66a5a252f0cb7
engsnayl <engsnayl@gmail.com>
2026-03-16 07:19:34 +0000

    Update main.tf

--- cloud-labs/lab-062-module-dependency/main.tf
+++ cloud-labs/lab-062-module-dependency/main.tf
@@ -1,4 +1,3 @@
-# Module Dependency Lab
 provider "aws" {
   region = "eu-west-2"
 }
@@ -10,8 +9,6 @@ module "vpc" {
 module "ec2" {
   source = "./modules/ec2"
 
-  # BUG 1: Wrong output reference
   vpc_id    = module.networking.vpc_id
-  # BUG 2: Wrong output name
   subnet_id = module.vpc.private_subnet
 }
