bce13b78fae8cebd34801cdc357646f4e50c17b2
engsnayl <engsnayl@gmail.com>
2026-04-13 07:19:38 +0100

    Update main.tf

--- cloud-labs/lab-076-route53-failover/main.tf
+++ cloud-labs/lab-076-route53-failover/main.tf
@@ -8,7 +8,6 @@ resource "aws_route53_zone" "main" {
 }
 
 resource "aws_route53_health_check" "primary" {
-  # BUG 1: Health check on wrong port
   fqdn              = "primary.example-internal.com"
   port               = 8080  # App is on port 80
   type               = "HTTP"
@@ -23,7 +22,7 @@ resource "aws_route53_record" "primary" {
   type    = "A"
   ttl     = 300
 
-  # BUG 2: Missing failover routing policy
+  # Missing failover routing policy
   # Should have: failover_routing_policy { type = "PRIMARY" }
   # set_identifier = "primary"
   # health_check_id = aws_route53_health_check.primary.id
@@ -35,10 +34,7 @@ resource "aws_route53_record" "secondary" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "app.example-internal.com"
   type    = "A"
-  # BUG 3: Very long TTL means slow failover
   ttl     = 3600
-
-  # BUG 4: Missing failover routing policy for secondary
   
   records = ["10.1.1.100"]
 }
