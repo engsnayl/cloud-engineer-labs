52fd5d612c7629018c5f46ad836ede24d1ba72c5
engsnayl <engsnayl@gmail.com>
2026-04-14 11:41:43 +0100

    Update main.tf

--- cloud-labs/lab-077-ecs-task-definition/main.tf
+++ cloud-labs/lab-077-ecs-task-definition/main.tf
@@ -61,8 +61,9 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "app" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-west-2a"
 }
 
 resource "aws_security_group" "app" {
