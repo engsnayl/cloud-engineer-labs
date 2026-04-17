d389deb0b8738d20d505b6013114d494821237b3
engsnayl <engsnayl@gmail.com>
2026-04-17 08:29:57 +0100

    Update main.tf

--- cloud-labs/lab-080-secrets-manager/main.tf
+++ cloud-labs/lab-080-secrets-manager/main.tf
@@ -1,11 +1,9 @@
-# Secrets Manager Lab
 provider "aws" {
-  region = "eu-west-2"
+  region = "eu-west-1"
 }
 
 resource "aws_secretsmanager_secret" "db_password" {
   name = "production/db-password"
-  # BUG 1: Rotation not enabled
 }
 
 resource "aws_secretsmanager_secret_version" "db_password" {
@@ -18,20 +16,3 @@ resource "aws_secretsmanager_secret_version" "db_password" {
     dbname   = "production"
   })
 }
-
-# BUG 2: Missing rotation configuration
-# resource "aws_secretsmanager_secret_rotation" "db_password" {
-#   secret_id           = aws_secretsmanager_secret.db_password.id
-#   rotation_lambda_arn = aws_lambda_function.rotation.arn
-#   rotation_rules {
-#     automatically_after_days = 30
-#   }
-# }
-
-# BUG 3: Missing Lambda function for rotation
-# The rotation Lambda needs:
-# - SecretsManager permissions
-# - VPC access to reach the database
-# - Correct IAM execution role
-
-# BUG 4: Missing resource policy allowing Lambda to get the secret
